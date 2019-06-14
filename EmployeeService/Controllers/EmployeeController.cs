460bcaf79b5868c14ce419d7ae82bd64389a581e
imarunya <31453894+imarunya@users.noreply.github.com>
2019-06-14 11:55:53 +0530

    Add project files.

---
+++ EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,126 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDB1Entities entities = new EmployeeDB1Entities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                using (EmployeeDB1Entities entities = new EmployeeDB1Entities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity != null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee with id " + id.ToString() + "is not found");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDB1Entities entity = new EmployeeDB1Entities())
+                {
+                    entity.Employees.Add(employee);
+                    entity.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDB1Entities entities = new EmployeeDB1Entities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee with id " + id.ToString() + "is not found");
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entities.Employees.FirstOrDefault(e => e.ID == id));
+                        entities.SaveChanges();
+                        return Request.CreateErrorResponse(HttpStatusCode.OK, "employee with id " + id.ToString() + "is removed");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+        }
+        public HttpResponseMessage Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDB1Entities entities = new EmployeeDB1Entities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee with id " + id.ToString() + "is not found");
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+
+                        entities.SaveChanges();
+                        return Request.CreateErrorResponse(HttpStatusCode.OK, "employee with id " + id.ToString() + "is updated");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+        }
+    }
+}
